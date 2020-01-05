08716ee94e3838044e91fe0e78ac700623ff1feb
Nito Buendia <effectedcard@gmail.com>
2020-01-05 15:26:47 +0800

    Create views.py for adding views to Hassio
    This component requires creating a view in order to capture the code from the OAuth call.

---
+++ custom_components/oura/views.py
@@ -0,0 +1,56 @@
+"""Defines Oura platform views."""
+
+from homeassistant import core
+from homeassistant.components import http
+import json
+
+# Views configuration.
+AUTH_CALLBACK_NAME = 'api:oura'
+AUTH_CALLBACK_PATH = '/oura/oauth/setup'
+
+
+class OuraAuthCallbackView(http.HomeAssistantView):
+  """Oura Authorization Callback View.
+
+  Methods:
+    get: Handles get requests to given view.
+  """
+
+  requires_auth = False
+  url = AUTH_CALLBACK_PATH
+  name = AUTH_CALLBACK_NAME
+
+  def __init__(self, sensor):
+    """Initializes view.
+
+    Args:
+      sensor: Sensor which initialized the OAuth process.
+    """
+    self._sensor = sensor
+
+  @core.callback
+  def get(self, request):
+    """Handles Oura OAuth callbacks.
+
+    Stores code from Oura API into cache token file.
+    This code will be read by the API and use it to retrieve access token.
+    """
+    code = request.query.get('code')
+    code_data = {'code': code}
+
+    sensor_name = request.query.get('state')
+    token_file_name = self._sensor._api.token_file_name
+
+    with open(token_file_name, 'w+') as token_file:
+      token_file.write(json.dumps(code_data))
+
+    self._sensor.update()  # Forces exchanging code for access token.
+
+    return self.json_message(
+        f'Oura OAuth code {code} for sensor.{sensor_name} stored in '
+        f'{token_file_name}. The sensor API will use this code to retrieve '
+        'the access token, store it and start fetching data on the next '
+        'update. No further action is required from your side. Any errors on '
+        'retrieving the API token will be logged. If you ever want to restart '
+        f'this OAuth process, simply delete the file {token_file_name} within '
+        'the /config/ directory.')
